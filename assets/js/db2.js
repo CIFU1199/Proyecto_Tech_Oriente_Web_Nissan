91d2243da8a31ceb3fb1299e29965c1d646ab5ef
CIFU1199 <edwkamilo@gmail.com>
2024-11-22 14:30:08 -0500

    Se agrego el modo showroom

---
+++ assets/js/db2.js
@@ -0,0 +1,13 @@
+export const cars = [
+    { name: "MARCH", image: "assets/img/carrusel/MARCH.png", url: "https://nos.dinissan.com.co/?vehicle=march&version=advance-at&colorSeleccionado=PLATA" },
+    { name: "NEW VERSA", image: "assets/img/carrusel/VERSA.png", url: "https://nos.dinissan.com.co/?vehicle=newVersa&version=sense-mt&colorSeleccionado=AZUL%20COBALTO" },
+    { name: "SENTRA", image: "assets/img/carrusel/SENTRA.png", url: "https://nos.dinissan.com.co/?vehicle=newSentra&version=exclusive&colorSeleccionado=PLATA" },
+    { name: "LEAF", image: "assets/img/carrusel/LEAF.png", url: "https://nos.dinissan.com.co/?vehicle=leaf&version=tekna&colorSeleccionado=BLANCO%20TECHO%20NEGRO" },
+    { name: "KICKS PLAY", image: "assets/img/carrusel/KICKS_PLAY.png", url: "https://nos.dinissan.com.co/?vehicle=kicks%20Play&version=sense-mt&colorSeleccionado=NARANJA" },
+    { name: "QASHQAI", image: "assets/img/carrusel/QASHQAI.png", url: "https://nos.dinissan.com.co/?vehicle=qashqai&version=sense-cvt&colorSeleccionado=ROJO" },
+    { name: "XTRAIL", image: "assets/img/carrusel/XTRAIL.png", url: "https://nos.dinissan.com.co/?vehicle=xtrail-epower&version=advance&colorSeleccionado=NEGRO" },
+    { name: "MURANO", image: "assets/img/carrusel/MURANO.png", url: "https://nos.dinissan.com.co/?vehicle=murano&version=exclusive&colorSeleccionado=ROJO%20PERLADO" },
+    { name: "PATHFINDER", image: "assets/img/carrusel/PATFINDER.png", url: "https://nos.dinissan.com.co/?vehicle=Pathfinder&version=EXCLUSIVE&colorSeleccionado=GRIS" },
+    { name: "FRONTIER DIESEL", image: "assets/img/carrusel/FRONTIERDIESEL.png", url: "https://nos.dinissan.com.co/?vehicle=frontier&version=DOBLE_CABINA4X4S-AT&colorSeleccionado=PLATA" },
+    { name: "FRONTIER GAS", image: "assets/img/carrusel/FRONTIERGAS.png", url: "https://nos.dinissan.com.co/?vehicle=frontierGasolina&version=DOBLE_CABINA4X2GASOLINA-GASOLINA&colorSeleccionado=ROJO" },
+];
